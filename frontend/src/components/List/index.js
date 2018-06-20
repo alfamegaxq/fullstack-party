530e99ee2d376264cf0baf1737a843bef3cae9e5
Gediminas Katilevicius <gediminas@waavo.com>
2018-06-21 00:28:23 +0300

    open closed issues styles

--- frontend/src/components/List/index.js
+++ frontend/src/components/List/index.js
@@ -8,7 +8,14 @@ export default class List extends Component {
             <div>
                 <Header/>
                 <div className="row m-0">
-                    <div className="col-md p-0"></div>
+                    <div className="col-md p-0">
+                        <div className="row">
+                            <div class="center-h">
+                                <span class="icon-open d-inline-block mr-1"></span><span>420 Open</span>
+                                <span class="icon-closed d-inline-block mr-1 ml-3"></span><span className="color-suva-grey">6.969 Closed</span>
+                            </div>
+                        </div>
+                    </div>
                     <div className={`${styles['image-container']} col-md p-0`}>
                         <div className={styles.background}></div>
                         <div className={`${styles['page-title']} center-h position-relative`}>
