ed54306a25a85808feb530bf07ddcd414156a5d6
Gediminas Katilevicius <gediminas@waavo.com>
2018-06-20 23:08:30 +0300

    login button style finishes

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -15,7 +15,7 @@ export default class Home extends Component {
                         </div>
                         <div className="row m-0">
                             <div className="col-sm p-0">
-                                <button className="btn full atlantis text-white rounded-5 mt-4">Login with GitHub</button>
+                                <button className="btn full atlantis text-white rounded-5 mt-4 hover-citrus border-0">Login with GitHub</button>
                             </div>
                         </div>
                     </div>
