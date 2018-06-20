09a940099655df266d2c4ba90b2a3e736ad5ad47
Gediminas Katilevicius <gediminas@waavo.com>
2018-06-21 00:19:39 +0300

    list page right image styles

--- frontend/src/components/List/index.js
+++ frontend/src/components/List/index.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import Header from "../Parts/Header";
+import styles from "./List.css";
 
 export default class List extends Component {
     render() {
         return (
             <div>
                 <Header/>
+                <div className="row m-0">
+                    <div className="col-md p-0"></div>
+                    <div className={`${styles['image-container']} col-md p-0`}>
+                        <div className={styles.background}></div>
+                        <div className={`${styles['page-title']} center-h position-relative`}>
+                            <h1>Full Stack Developer Task <small className="center-h mt-3">by <img src="/img/xsmall-logo.png" alt="testio"/></small></h1>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
