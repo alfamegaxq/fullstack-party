f45a0eec260da672e8663964a1ef5d632fb78a99
Gediminas Katilevicius <gediminas@waavo.com>
2018-06-20 23:03:16 +0300

    home screen styles

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react';
-import './Home.css';
+import React, {Component} from 'react';
+import styles from './Home.css';
 
 export default class Home extends Component {
     render() {
         return (
-            <div className="home-container">
-                <div className="home-background"></div>
+            <div className={styles.container}>
+                <div className={styles.background}></div>
+                <div className={`${styles['login-box']} center-h position-relative`}>
+                    <div className="container p-0">
+                        <div className="row m-0">
+                            <div className="col-sm p-0">
+                                <img src="/img/logo.png" className="center-h mb-4"/>
+                            </div>
+                        </div>
+                        <div className="row m-0">
+                            <div className="col-sm p-0">
+                                <button className="btn full atlantis text-white rounded-5 mt-4">Login with GitHub</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
