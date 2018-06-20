046ce84aeaaa6fd247c2cceb5afdd67cf540a46c
Gediminas Katilevicius <gediminas@waavo.com>
2018-06-21 00:53:33 +0300

    prototyped details page styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from './components/Home';
 import List from "./components/List";
+import Details from "./components/Details";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <List/>
+        <Details/>
       </div>
     );
   }
---
+++ frontend/src/components/Details/index.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import Header from "../Parts/Header";
+import styles from "./Details.css";
+
+export default class Details extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <div className="container">
+                    <div className="row mt-3 mb-3">
+                        <span className="icon-back"></span> Back to Issues
+                    </div>
+                    <div className="row">
+                        <div className="card">
+                            <h1>Deprecate Symfony environemnt variables <span className="task-nr">#000001</span></h1>
+                            <br/>
+                            <div className="btn"><span className="icon-open d-inline-block"></span>OPEN</div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-1"></div>
+                        <div className="col-sm">
+                            <div className="card"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
